774e706a318e42b328a5f5ba611ba5b209057c6a
Nandhakumar <39767305+nandhukumar86@users.noreply.github.com>
2021-07-06 21:19:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 import gradio as gr
 import pydicom
 import io
+import tensorflow
+from tensorflow.keras.applications.densenet import preprocess_input
+import cv2
+import numpy as np
+
+path = 'model-dn.h5'
+model = tensorflow.keras.models.load_model(path)
 
 def detectPneumonia(file):
   f = open(file.name, mode='rb')
   raw_bytes = f.read()
   ds = pydicom.dcmread(io.BytesIO(raw_bytes), force = True)
   f.close();
+  
+  input_image = cv2.cvtColor(cv2.resize(ds.pixel_array,(256,256)), cv2.COLOR_BAYER_GR2RGB)
+  X = []
+  X.append(input_image)
+  X = preprocess_input(np.array(X))
+
+  output_classify, output_segment =  model.predict(X)
+  
+  output_segment = output_segment[0].reshape(64,64,3)
+  output_classify = "Not Identified" if output_classify[0].argmax() == 1 else "Identified"
+  
   imagedata = {
       "Patient ID" : ds.PatientID,
       "Age" : ds.PatientAge,
       "Gender" : ds.PatientSex,
       "Body Part" : ds.BodyPartExamined
   }
-  return imagedata, ds.pixel_array
+  return imagedata, ds.pixel_array, output_segment, output_classify
 
 iface = gr.Interface(fn = detectPneumonia, 
                      inputs = [
@@ -21,7 +39,9 @@ iface = gr.Interface(fn = detectPneumonia,
                      ], 
                      outputs = [
                               gr.outputs.KeyValues(label="Patient Details"),
-                              gr.outputs.Image(label="Loaded Image")
+                              gr.outputs.Image(label="Loaded Image"),
+                              gr.outputs.Image(label="Segmented Image"),
+                              gr.outputs.Label(label="Is Detected"),
                      ],
                      layout = "vertical",
                      title = "Pneumonia Detection",
