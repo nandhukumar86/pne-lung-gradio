5e0aa0734b4a9012b87654044c09e5129761dcba
Nandhakumar <Nandhakumar>
2021-07-08 16:02:05 +0530

    updated with new model

--- app.py
+++ app.py
@@ -4,9 +4,22 @@ import io
 import tensorflow
 import cv2
 import numpy as np
+from tensorflow.keras import backend as K
+from tensorflow.keras.backend import log,epsilon
 
-path = 'model-dn.h5'
-model = tensorflow.keras.models.load_model(path)
+def dice_coef(y_true, y_pred, smooth=1):
+  intersection = K.sum(y_true * y_pred, axis=[1])
+  union = K.sum(y_true, axis=[1]) + K.sum(y_pred, axis=[1])
+  dice = K.mean((2. * intersection + smooth)/(union + smooth), axis=0)
+  return dice
+
+def loss(y_true, y_pred):
+    return 1 - (dice_coef(y_true, y_pred) + epsilon())
+
+custom_objects = {"dice_coef": dice_coef}
+
+path = '/content/drive/MyDrive/CapstoneDataSet/modelPickle/model-dn-v1.h5'
+model = tensorflow.keras.models.load_model(path, custom_objects=custom_objects)
 
 def detectPneumonia(file):
   f = open(file.name, mode='rb')
@@ -21,7 +34,7 @@ def detectPneumonia(file):
 
   output_classify, output_segment =  model.predict(X)
   
-  output_segment = output_segment[0].reshape(64,64,3).astype('int')
+  output_segment = output_segment[0].reshape(64,64)
   output_classify = "Not Identified" if output_classify[0].argmax() == 1 else "Identified"
   
   imagedata = {
